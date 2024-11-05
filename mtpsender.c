2c70d1f8c85f9bafbaed6d440396ecbfcfb9ffd4
dburnaye <dburnaye@gmu.edu>
2024-11-05 15:54:42 -0500

    Error Message Function

--- mtpsender.c
+++ mtpsender.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <time.h>
 #include <sys/socket.h>
@@ -7,38 +8,54 @@
 
 #include "mtppacket.h"
 
+#define MAX_ERROR_LENGTH 300
+
+void closeImportantFiles(FILE* inputFile, FILE* logFile);
+int endWithErrorMessage(int messageParts, ...);
+int isCorrupt();
+
 int main(int argc, char** argv) {
 
     FILE* inputFile;
     FILE* logFile;
     
     if (argc < 5 || argc > 6) {
-        fprintf(stderr, "There needs to be 5 to 6 parameters! Follow the format below:\n\
-        ./mtpsender <receiver-IP> <receiver-port> <window-size>	<input-file> <sender-logfile>\n\
-        If <sender-logfile> isn't provided, a file called mtpsender_logs.txt will be created in\
-         the firectory this file is in.\n");
-        
-        return 0;
+        int error = endWithErrorMessage(1, "There needs to be 5 to 6 parameters! Follow the format below:\n\
+./mtpsender <receiver-IP> <receiver-port> <window-size>	<input-file> <sender-logfile>\n\
+If <sender-logfile> isn't provided, a file called mtpsender_logs.txt will be created in\
+the directory this file is in.\n");
+         return error;
     }
     else {
         inputFile = fopen(argv[4], "r");
         logFile = fopen((argc == 6) ? argv[5] : "mtpsender_logs.txt", "w");
-        fprintf(stderr, "%s%s", ((!inputFile) ? "Something wrong happened with the input file!\n" : ""), ((!logFile) ? "Something wrong happened with the log file!\n" : ""));
 
         if (!inputFile || !logFile) {
             closeImportantFiles(inputFile, logFile);
-            return 0;
+            // fprintf(stderr, "%s%s", ((!inputFile) ? "Something wrong happened with the input file!\n" : ""), ((!logFile) ? "Something wrong happened with the log file!\n" : ""));
+            // return 0;
+
+            return endWithErrorMessage(2, 
+                        ((!inputFile) ? "Something wrong happened with the input file!\n" : ""), 
+                        ((!logFile) ? "Something wrong happened with the log file!\n" : ""));
         }
     }
 
-    //open up a UDP client socket
+    //opens up a UDP client socket (NEED TO WORK ON THIS)
     int senderSocket;
     if ((senderSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        fprintf(stderr, "Socket couldn't be created!\n");
-        return 0;
+        int error = endWithErrorMessage(1, "Socket couldn't be created!\n");
+        return error;
     }
 
+
     //sender needs to read the file
+    char messageBuffer[MAX_DATA_LENGTH];
+    if (fgets(messageBuffer, MAX_DATA_LENGTH, inputFile)) {
+        
+    }
+
+
     //creates and sends DATA packets (type = 1) to a receiver
     //receives ACK packets from receiver
 
@@ -49,6 +66,20 @@ int main(int argc, char** argv) {
 void closeImportantFiles(FILE* inputFile, FILE* logFile) {
     fclose(inputFile);
     fclose(logFile);
+}
+
+int endWithErrorMessage(int messageParts, ...) {
+    va_list messageList;
+    va_start(messageList, messageParts);
+
+    char* errorMessage = (char*)malloc(sizeof(char) * (MAX_ERROR_LENGTH + 1));
+    int counter;
+    for (counter = 0; counter < messageParts; counter++) {
+        strcat(errorMessage, va_arg(messageList, const char*));
+    }
+    
+    fprintf(stderr, "%s", errorMessage);
+    return 0;
 }
 
 // do checksub checking in here
